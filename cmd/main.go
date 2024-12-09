204de5773f655f12ccc99ef8a6afb96993480eea
Keven Marques <99088928+M3G4DeaTh@users.noreply.github.com>
2024-12-09 16:36:24 -0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"go-api/controller"
+	db "go-api/infrastructure"
+	"go-api/repository"
+	"go-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	dbConnection, err := db.ConnectDB()
+	ProductRepository := repository.NewProductRepository(dbConnection)
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+
+	if err != nil {
+		panic(err)
+	}
+	ProductController := controller.NewProductController(ProductUseCase)
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	server.GET("/products", ProductController.GetProducts)
+	server.Run(":8080")
+}
